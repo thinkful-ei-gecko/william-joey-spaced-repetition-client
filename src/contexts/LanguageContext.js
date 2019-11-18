e24408113e3fe0e1e43d91a48d10f88c038153d8
Joey Romo <joeyjr95@gmail.com>
2019-11-18 10:56:43 -0500

    context

---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import LanguageService from '../services/language-service'
+
+const LanguageContext = React.createContext({
+})
+export default LanguageContext;
