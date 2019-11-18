90dc1450b2cba00b250ef6c118ebb42f42b7b80f
William Bae <wbae912@gmail.com>
2019-11-18 09:44:46 -0800

    Working on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,27 +7,31 @@ export default class Dashboard extends React.Component {
     
     componentDidMount() {
         LanguageService.getLanguageAndWords()
-            .then(words => {
-                this.context.setWords(words)
+            .then(data => {
+                console.log(data.language);
+                this.context.setWords(data.words);
+                this.context.setLanguage(data.language);
             })
             .catch(res => {
-                this.context.setError(res.error)
+                this.context.setError(res.error);
             })
     }
 
     render() {
-        const { words = [] } = this.context;
+        const { words = [], language = {} } = this.context;
         return (
             <>
-                <h2 id="words-h2">Words</h2>
-
+                <h2 className="language-h2">{language.name}</h2>
+                <h3 id="words-h3">Words</h3>
+    
                 {words.map(word =>
                     <ul className='words-list' key={word.id}>
                         <li className='word-li'>{word.translation}</li>
                         <li className='word-li'>{word.original}</li>
+                        <li className='word-li'>{word.correct_count}</li>
+                        <li className='word-li'>{word.incorrect_count}</li>
                     </ul>
                 )}
-
             </>
         )
     }
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react'
 
 
 const LanguageContext = React.createContext({
+    language: {},
     words: [],
     error: null,
     setError: () => {},
-    setWords: () => {}
+    setWords: () => {},
+    setLanguage: () => {}
 })
 
 export default LanguageContext;
 
 export class LanguageProvider extends Component {
     state = {
+        language: {},
         words: [],
         error: null,
     }
+    setLanguage = language => {
+        this.setState({ language })
+    }
     setWords = words =>{
-        this.setState({words})
+        this.setState({ words })
     }
     setError = error => {
         console.error(error)
@@ -24,10 +30,12 @@ export class LanguageProvider extends Component {
       }
       render() {
           const value = {
+              language: this.state.language,
               words: this.state.words,
               error: this.state.error,
               setError: this.setError,
               setWords: this.setWords,
+              setLanguage: this.setLanguage
           }
           return (
             <LanguageContext.Provider value={value}>
