bf13ce312860bed59053d4f076392f1c36e5ce9e
Joey Romo <joeyjr95@gmail.com>
2019-11-20 14:06:26 -0500

    context

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -11,6 +11,7 @@ const LanguageContext = React.createContext({
     setLanguage: () => {},
     setHeadWord: () => {},
     setResponse: () => {},
+    setGuess: () => {},
     guess: '',
     response: {}
 })
